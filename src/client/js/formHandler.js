089179eab0e41d6f6d69ae13a9dc2f1022a01790
Arya Murthi <aryamurthi@Aryas-MBP.fios-router.home>
2020-07-07 17:05:58 -0400

    commit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,19 +1,42 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
+  // check what text was put into the form field
+  let formText = document.getElementById("name").value;
 
-    Client.checkForName(formText)
+  Client.checkForName(formText);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8082/test')
-    .then(res => {
-        return res.json()
+  console.log("::: Form Submitted :::");
+  fetch("http://localhost:8082/test")
+    .then((res) => {
+      return res.json();
     })
-    .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
+    .then(function (data) {
+      document.getElementById("results").innerHTML = data.title;
     })
+    .then(
+      getWeather(
+        "https://api.openweathermap.org/data/2.5/weather?zip=01581,us&appid=5d440dcd25ae78fd3e21200458636bba&units=imperial"
+      ).then(function (response) {updateUI(response)})
+    );
 }
 
-export { handleSubmit }
+const getWeather = async (url) => {
+  const res = await fetch(url);
+  try {
+    const response = await res.json();
+    return response;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+const updateUI = async (response) => {
+  try {
+    document.getElementById("response").innerHTML = response.main.temp;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,37 +1,49 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-var bodyParser = require('body-parser')
-var cors = require('cors')
+const dotenv = require("dotenv").config();
+console.log(process.env.API_ID);
+
+var path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+var aylien = require("aylien_textapi");
+
+var textapi = new aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY,
+});
 
 var json = {
-    'title': 'test json response',
-    'message': 'this is a message',
-    'time': 'now'
-}
+  title: "test json response",
+  message: "this is a message",
+  time: "now",
+};
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 // to use json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // to use url encoded values
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-console.log(JSON.stringify(mockAPIResponse))
+console.log(JSON.stringify(mockAPIResponse));
 
-app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
+app.get("/", function (req, res) {
+  res.sendFile("dist/index.html");
+});
 
-app.get('/test', function (req, res) {
-    res.json(mockAPIResponse);
-})
+app.get("/test", function (req, res) {
+  res.json(mockAPIResponse);
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8082, function () {
-    console.log('Example app listening on port 8082!')
-})
+  console.log("Example app listening on port 8082!");
+});
